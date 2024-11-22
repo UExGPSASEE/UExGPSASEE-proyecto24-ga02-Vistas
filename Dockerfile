5bb063ce5e2f4b4266a54754d9ed1b6b8b08d59b
Jesus Santiago <Sanjesvel@outlook.com>
2024-11-22 15:42:30 +0100

    Api_Vistas

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3-alpine
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+COPY requirements.txt /usr/src/app/
+COPY requeriments_sqlalchemy.txt /usr/src/app/
+
+RUN pip3 install --no-cache-dir -r requirements.txt
+RUN pip3 install --no-cache-dir -r requeriments_sqlalchemy.txt
+
+COPY . /usr/src/app
+
+EXPOSE 8082
+
+ENTRYPOINT ["python3"]
+
+CMD ["-m", "openapi_server"]
---
+++ openapi_server/__main__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import connexion
+from openapi_server import encoder
+from flask_sqlalchemy import SQLAlchemy
+from openapi_server.controllers.vista_controller import import_db_controller
+from openapi_server.models.vista import import_db
+
+from flask_cors import CORS
+
+# commit test
+app = connexion.App(__name__, specification_dir='./openapi/')
+CORS(app.app)
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('openapi.yaml',
+            arguments={'title': 'Microservicio de Contenidos de una aplicación de tipo Netflix'},
+            pythonic_params=True)
+
+app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345@localhost:5432/Vistas'
+app.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    'pool_size': 30,        # Tamaño máximo de conexiones en el pool
+    'pool_timeout': 30,     # Tiempo máximo de espera para obtener una conexión
+    'pool_recycle': 180,   # Tiempo máximo de vida de una conexión (en segundos)
+    'max_overflow': 5       # Conexiones extra que pueden crearse si se alcanza el pool_size
+}
+# hola 
+
+db = SQLAlchemy(app.app)
+
+import_db_controller(db)
+import_db(db)
+
+app.run(port=8082)
